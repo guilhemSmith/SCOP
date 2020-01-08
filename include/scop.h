9be3a38ead6c8bda4d35772567e91219191935c6
Guilhem Smith <gsmith@student.42.fr>
2020-01-08 16:59:46 +0100

    Core logic for main loop.

--- include/scop.h
+++ include/scop.h
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 13:44:02 by gsmith            #+#    #+#             */
-/*   Updated: 2020/01/08 16:24:50 by gsmith           ###   ########.fr       */
+/*   Updated: 2020/01/08 16:52:50 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define SCOP_H
 
 # define GL_SILENCE_DEPRECATION
+# define WIDTH_DEF 600
+# define HEIGHT_DEF 800
 
 # ifndef DEP
 #  include <GLFW/glfw3.h>
--- src/main.c
+++ src/main.c
@@ -6,13 +6,20 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 13:43:48 by gsmith            #+#    #+#             */
-/*   Updated: 2020/01/08 16:40:00 by gsmith           ###   ########.fr       */
+/*   Updated: 2020/01/08 16:58:47 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.h"
 
-int		main(void)
+void				framebuffer_size_callback(GLFWwindow *window, \
+						int width, int height)
+{
+	(void)window;
+	glViewport(0, 0, width, height);
+}
+
+static GLFWwindow	*init_opengl(void)
 {
 	GLFWwindow	*window;
 
@@ -21,17 +28,42 @@ int		main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(HEIGHT_DEF, WIDTH_DEF, "Hello World", NULL, NULL);
 	if (window == NULL)
 	{
 		ft_putendl_fd("Failed to create GLFW window", 2);
 		glfwTerminate();
-		return (-1);
+		return (NULL);
 	}
 	glfwMakeContextCurrent(window);
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, HEIGHT_DEF, WIDTH_DEF);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	return (window);
+}
+
+static void			process_input(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, 1);
+}
+
+static void			process_render(GLFWwindow *window)
+{
+	(void)window;
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int					main(void)
+{
+	GLFWwindow	*window;
+
+	if ((window = init_opengl()) == NULL)
+		return (-1);
 	while (!glfwWindowShouldClose(window))
 	{
+		process_input(window);
+		process_render(window);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
