28a9a02c51b297dda6fdfe7dbdfa39f8a63a56e0
Guilhem Smith <gsmith@student.42.fr>
2020-02-20 09:51:05 +0100

    Flat shader pick a color for each triangle.

--- ressources/shaders/fragment_flat.glsl
+++ ressources/shaders/fragment_flat.glsl
@@ -1,11 +1,12 @@
 #version 400 core
 out vec4 frag_color;
 
-flat in vec3 frag_pos;
+in vec3 frag_pos;
+flat in vec3 frag_col;
 
 uniform vec3 object_color;
 
 void main()
 {
-	frag_color = vec4(object_color, 1.0);
+	frag_color = vec4(frag_col, 1.0);
 }
--- ressources/shaders/vertex_flat.glsl
+++ ressources/shaders/vertex_flat.glsl
@@ -1,7 +1,8 @@
 #version 400 core
 layout (location = 0) in vec3 a_pos;
 
-flat out vec3 frag_pos;
+out vec3 frag_pos;
+flat out vec3 frag_col;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,4 +12,10 @@ void main()
 {
 	frag_pos = vec3(view * model * vec4(a_pos, 1.0));
 	gl_Position = projection * view * model * vec4(a_pos, 1.0);
+
+    float r, g, b;
+    r = (int(a_pos.x * 500) % 255) / 255.0f;
+    g = (int(a_pos.y * 500) % 255) / 255.0f;
+    b = (int(a_pos.z * 500) % 255) / 255.0f;
+    frag_col = vec3(r, g, b);
 }
